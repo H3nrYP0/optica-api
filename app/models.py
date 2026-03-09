1b5a49c09401dd254556574a48f9e8240a703ccc
H3nrYP0 <david13posso@gmail.com>
2026-03-09 13:06:09 -0500

    Cositas...

--- app/models.py
+++ app/models.py
@@ -280,10 +280,10 @@ class DetalleCompra(db.Model):
 class Servicio(db.Model):
     __tablename__ = 'servicio'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(20), nullable=False)
+    nombre = db.Column(db.String(65), nullable=False)
     duracion_min = db.Column(db.Integer, nullable=False)
     precio = db.Column(db.Float, nullable=False)
-    descripcion = db.Column(db.String(120))
+    descripcion = db.Column(db.String(200))
     estado = db.Column(db.Boolean, default=True)
     citas = db.relationship('Cita', backref='servicio', lazy=True)
 
